11360e91f311b54d1687f76a4694fdf891417fe2
faisal ejaz <faisal@faisal-laptop.(none)>
2010-12-28 21:16:47 +0500

    file name changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -337,9 +337,6 @@ class Product < ActiveRecord::Base
       ext_array = [".csv"]
       file_name= file.original_filename
       directory= "#{RAILS_ROOT}/public/input_files" #local directory path
-      puts "stttttttttttttttttttarttttttttt"
-      puts RAILS_ROOT
-      puts "ennnnnnnnnnnnnnnnnndddddddddddddddd"
       ext = File.extname(file_name)
       ext = ext.downcase
       if ext_array.include?(ext)
