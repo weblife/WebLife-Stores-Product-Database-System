c5112d3efa20b24ecebcde39b46c2fb2388a435e
Eugen Minciu <eugen@lesseverything.com>
2011-01-10 20:10:24 +0200

    compscraper comments

--- app/models/compscraper.rb
+++ app/models/compscraper.rb
@@ -24,32 +24,6 @@
 #  updated_at         :datetime
 #
 
-# == Schema Information
-#
-# Table name: compscrapers
-#
-#  id                  :integer(4)      not null, primary key
-#  user_id             :integer(4)      not null
-#  item_search_phrase  :string(255)
-#  word_that_occur     :string(255)
-#  sort_options        :string(255)
-#  lowest_price        :decimal(8, 2)   default(0.0)
-#  decimal             :decimal(8, 2)   default(0.0)
-#  second_lowest_price :decimal(8, 2)   default(0.0)
-#  third_lowest_price  :decimal(8, 2)   default(0.0)
-#  fourth_lowest_price :decimal(8, 2)   default(0.0)
-#  fifth_lowest_price  :decimal(8, 2)   default(0.0)
-#  sixth_lowest_price  :decimal(8, 2)   default(0.0)
-#  lowest_site         :string(255)
-#  second_lowest_site  :string(255)
-#  third_lowest_site   :string(255)
-#  fourth_lowest_site  :string(255)
-#  fifth_lowest_site   :string(255)
-#  sixth_lowest_site   :string(255)
-#  created_at          :datetime
-#  updated_at          :datetime
-#
-  require 'fastercsv'
 class Compscraper < ActiveRecord::Base
 
   validates_length_of :item_search_phrase,:allow_nil => true,:maximum   => 100,:too_long  => "Search item phrase must have at most 100 characters"
